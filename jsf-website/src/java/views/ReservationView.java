d07b708c72993454249be13cce97506fd7e08df3
Valentin Zmiycharov <valentin.zmiycharov@gmail.com>
2016-04-24 14:57:43 +0300

    Implement reservation

--- jsf-website/src/java/views/ReservationView.java
+++ jsf-website/src/java/views/ReservationView.java
@@ -5,10 +5,19 @@
  */
 package views;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import javax.annotation.PostConstruct;
 import javax.inject.Named;
 import javax.enterprise.context.Dependent;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import org.primefaces.event.SelectEvent;
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.CategoryAxis;
+import org.primefaces.model.chart.ChartSeries;
+import org.primefaces.model.chart.LineChartModel;
 
 /**
  *
@@ -70,5 +79,63 @@ public class ReservationView {
 
     public String getAdditionalInformation() {
         return additionalInformation;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDateFrom(Date dateFrom) {
+        this.dateFrom = dateFrom;
+    }
+
+    public void setDateTo(Date dateTo) {
+        this.dateTo = dateTo;
+    }
+
+    public void setRoomKind(String roomKind) {
+        this.roomKind = roomKind;
+    }
+
+    public void setAdultsCount(int adultsCount) {
+        this.adultsCount = adultsCount;
+    }
+
+    public void setChildrenCount(int childrenCount) {
+        this.childrenCount = childrenCount;
+    }
+
+    public void setHasBreakfast(boolean hasBreakfast) {
+        this.hasBreakfast = hasBreakfast;
+    }
+
+    public void setHasSPA(boolean hasSPA) {
+        this.hasSPA = hasSPA;
+    }
+
+    public void setAdditionalInformation(String additionalInformation) {
+        this.additionalInformation = additionalInformation;
+    }
+    
+    @PostConstruct
+    public void init() {
+        adultsCount = 1;
+        roomKind = "Двойна стая";
+    }
+    
+    public String preview() {
+        return "reservationsPreview";
+    }
+    
+    public String finish() {
+        return "reservationsFinish";
     }
 }
